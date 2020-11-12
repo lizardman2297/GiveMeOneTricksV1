a22a340a0bfd6264adc3faffc9a117699c5104c3
lizardman <34143642+lizardman97@users.noreply.github.com>
2020-11-12 22:56:40 +0100

    menu css

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,7 +1,11 @@
 function switchDrop() {
-    if (document.getElementById("drop").style.visibility == "visible") {
-        document.getElementById("drop").style.visibility = "hidden"
-    }else if (document.getElementById("drop").style.visibility == "hidden" || document.getElementById("drop").style.visibility == "") {
-        document.getElementById("drop").style.visibility = "visible"
+    if (document.getElementById("drop").classList.contains("dropDisp")) {
+        document.getElementById("drop").classList.add("dropHide")
+        document.getElementById("drop").classList.remove("dropDisp")
+        document.getElementsByClassName("menu")[0].style.zIndex = 0
+    }else if (document.getElementById("drop").classList.contains("dropHide")) {
+        document.getElementById("drop").classList.remove("dropHide")
+        document.getElementById("drop").classList.add("dropDisp")
+        document.getElementsByClassName("menu")[0].style.zIndex = 10
     }
 }
